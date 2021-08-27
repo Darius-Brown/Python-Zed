95d1b2ed34abc2046e450314670ec60df88d020e
Darius Brown <Brown.darius.r@gmail.com>
2021-08-27 17:22:04 -0500

    numbers

--- hello.py
+++ hello.py
@@ -1,1 +1,4 @@
-print("Hello, World!");                                      
+print("Hello, World!");  
+#print("octothorpe");  
+print("I will now count my chickens.");
+print("Hens", 25 + 30 /6 );                                  
