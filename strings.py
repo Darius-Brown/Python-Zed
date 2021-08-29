f12232cb557bd59d81375d32a527b0817e01869c
Darius Brown <Brown.darius.r@gmail.com>
2021-08-28 20:27:59 -0500

    py

---
+++ strings.py
@@ -0,0 +1,11 @@
+x = "There are  %d types of people" % 10
+binary = "binary"
+do_not = "don't"
+
+y = "thos who know %s, and those who %s" % (binary, do_not)
+
+print(x)
+print(y)
+
+print("I said: %r " % (x))
+print(" I also said: '%s'" % (y))
